7990758b03057a50f7d6d25e5bd5834bac761c92
Daijona Revell <daidai@Daijonas-MacBook-Air.local>
2020-08-08 22:18:55 -0400

    Added deposit/Withdraw

--- src/com/codeintelx/bank/models/Account.java
+++ src/com/codeintelx/bank/models/Account.java
@@ -2,5 +2,59 @@ package com.codeintelx.bank.models;
 
 public class Account
 {
+    private String accountNumber;
+    private double balance;
+    private String customerName;
+    private String typeOfAccount;
+
+    public Account(String accountNumber, double balance, String customerName, String typeOfAccount)
+    {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.typeOfAccount = typeOfAccount;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getTypeOfAccount() {
+        return typeOfAccount;
+    }
+
+    public void setTypeOfAccount(String typeOfAccount) {
+        this.typeOfAccount = typeOfAccount;
+    }
+
+    //Name
+
+
+
+    //Type of Account
+
+
+
+    //Account customer name and account ID
 
 }
--- src/com/codeintelx/bank/services/AccountServices.java
+++ src/com/codeintelx/bank/services/AccountServices.java
@@ -3,4 +3,35 @@ package com.codeintelx.bank.services;
 public class AccountServices
 {
 
+    private double balance;
+
+    public AccountServices(double balance)
+    {
+        this.balance = balance;
+    }
+
+    //Deposit amount
+
+
+    public void depositFunds(double deposit)
+    {
+        this.balance+=deposit;
+        System.out.println(this.balance);
+
+    }
+
+    //Withdraw from amount
+
+    public void withdrawFunds(double withdraw)
+    {
+        this.balance -= withdraw;
+        if (this.balance < 0) {
+            System.out.println("Insufficient Funds");
+
+        } else {
+            System.out.println(this.balance);
+        }
+
+    }
+
 }
